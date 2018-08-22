3d4f02a3cb31ac40613f7c276c51d89e8da86e8a
Ken Ronoh <kennethronoh1@gmail.com>
2018-08-22 15:14:17 -0700

    Create form.php
    This is the form fileds.

---
+++ form.php
@@ -0,0 +1,9 @@
+<form action="emailer.php" method="post">
+							<input type="text"name="name" placeholder="Name">
+							<input type="text" name="phone" placeholder="Phone">
+							<input type="text" name="email"  placeholder="Email">
+							<textarea placeholder="Message" name="message" required="true"></textarea>
+							<div class="submit-btn">
+								<input type="submit" value="Send">
+							</div>
+						</form>
